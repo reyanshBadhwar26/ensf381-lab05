950b0d9b7f85ceadd0dedb8fc9718dd695ec0268
Gooogllleee <129225137+reyanshBadhwar26@users.noreply.github.com>
2026-03-06 12:01:01 -0700

    lab done

--- jsx-exercise/src/App.js
+++ jsx-exercise/src/App.js
@@ -4,7 +4,6 @@ import Controls from './Controls';
 import UserList from './UserList';
 
 function App() {
-  let currentYear = new Date().getFullYear();
   let isLoggedIn = true;
   if (isLoggedIn) {
     return (
@@ -21,20 +20,20 @@ function App() {
       </div>
     );
   }
+}
 
 // EXERCISE 1 CODE!!
-  //function App() {
-    // let currentYear = new Date().getFullYear();
-    // let isLoggedIn = true;
-    // return (
-    //   <div>
-    //     <h1>ENSF-381: Full Stack Web Development</h1>
-    //     <p>React Components</p>
-    //     <p>Current Year: {currentYear}</p>
-    //     <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
-    //   </div>
-    // );
-    //}
-}
+// function App() {
+//   let currentYear = new Date().getFullYear();
+//   let isLoggedIn = true;
+//   return (
+//     <div>
+//       <h1>ENSF-381: Full Stack Web Development</h1>
+//       <p>React Components</p>
+//       <p>Current Year: {currentYear}</p>
+//       <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
+//     </div>
+//   );
+//   }
 
 export default App;
